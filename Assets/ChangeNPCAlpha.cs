1db596bab5ce3b6e4f54e9285b029c3cba5bd733
Piotr <janek935@gmail.com>
2017-07-12 14:47:24 +0200

    NPC model and behaviour

---
+++ Assets/ChangeNPCAlpha.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeNPCAlpha : MonoBehaviour {
+
+    public Transform player;
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float playerD = Vector3.Distance(player.position, transform.position);
+
+        if (playerD < 10)
+        {
+            float alpha = 0;
+            Renderer renderer = GetComponent<Renderer>();
+            Color color = renderer.material.color;
+            color.a = alpha;
+            renderer.material.color = color;
+        }
+        else if (playerD < 30)
+        {
+            float alpha = (playerD-10) / 20;
+            Renderer renderer = GetComponent<Renderer>();
+            Color color = renderer.material.color;
+            color.a = alpha;
+            renderer.material.color = color;
+        }
+    }
+}
---
+++ Assets/NPCAI.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCAI : MonoBehaviour {
+
+    public Transform player;
+
+    private List<Transform> poi;
+    private int currentPOI = 0;
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    private bool idle = false;
+    private float timer = 0;
+    private float idleLimiter = 3;
+    private static System.Random random = new System.Random();
+
+    // Use this for initialization
+    void Start () {
+        GameObject poiContener = GameObject.Find("POI");
+        int childCount = poiContener.transform.childCount;
+        poi = new List<Transform>();
+        for (int i=0; i<childCount; i++)
+        {
+            poi.Add(poiContener.transform.GetChild(i));
+        }
+
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        currentPOI = random.Next(0, poi.Count);
+        agent.speed = 7;
+        animator.SetBool("Walk", true);
+        animator.SetBool("Run", false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (idle)
+        {
+            timer += Time.deltaTime;
+            if (timer >= idleLimiter)
+            {
+                timer = 0;
+                idle = false;
+                animator.SetBool("Idle", false);
+                animator.SetBool("Walk", true);
+                return;
+            }
+        }
+        else
+        {
+            agent.destination = poi[currentPOI].position;
+            float d = Vector3.Distance(poi[currentPOI].position, transform.position);
+            if (d < 5)
+            {
+                currentPOI = random.Next(0, poi.Count);
+                if (currentPOI >= poi.Count)
+                {
+                    currentPOI = 0;
+                }
+                idle = true;
+                idleLimiter = (float)(random.NextDouble() * 5 + 3);
+                animator.SetTrigger("Idle");
+                animator.SetBool("Walk", false);
+            }
+        }
+
+        
+
+        
+
+        //agent.destination = player.position;
+
+        //float d = Vector3.Distance(player.position, transform.position);
+        //if (d > 20)
+        //{
+        //    agent.speed = 20;
+        //    animator.SetBool("Run", true);
+        //    animator.SetBool("Walk", false);
+        //}
+        //else
+        //{
+        //    agent.speed = 10;
+        //    animator.SetBool("Walk", true);
+        //    animator.SetBool("Run", false);
+        //}
+
+        //if (d< 10)
+        //{
+        //    agent.speed = 0;
+        //    animator.SetTrigger("Punch");
+        //}
+    }
+}
