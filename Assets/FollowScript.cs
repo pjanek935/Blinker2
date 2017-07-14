d84e6d269d72bbf17e85d404fedc8ebe317053e9
Piotr <janek935@gmail.com>
2017-07-14 18:25:04 +0200

    Idle animations + tutorial scene

---
+++ Assets/FollowScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowScript : MonoBehaviour {
+
+    public Transform target;
+    public float deltaX = 0;
+    public float deltaY = 0;
+    public float deltaZ = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.position + new Vector3(deltaX, deltaY, deltaZ);
+	}
+}
