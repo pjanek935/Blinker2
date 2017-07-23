93856b0001cc26ba96b325d07158395336f3ddbe
Piotr <janek935@gmail.com>
2017-07-23 14:27:36 +0200

    Level 1 Rebulit + Light Switch

---
+++ Assets/Scripts/CeilingLampAction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilingLampAction : SwitchAction {
+
+    public GameObject cilinder;
+    public Light light;
+
+    private bool isOn = true;
+
+    public override void Action()
+    {
+        if (isOn)
+            TurnOff();
+        else
+            TurnOn();
+    }
+
+    void TurnOn()
+    {
+        isOn = true;
+        Renderer renderer = cilinder.GetComponent<Renderer>();
+        Material mat = renderer.material;
+        mat.SetColor("_EmissionColor", Color.white);
+        light.intensity = 8;
+    }
+
+    void TurnOff()
+    {
+        isOn = false;
+        Renderer renderer = cilinder.GetComponent<Renderer>();
+        Material mat = renderer.material;
+        mat.SetColor("_EmissionColor", Color.black);
+        light.intensity = 0;
+    }
+}
---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+    private bool playerInside = false;
+    public SwitchAction action;
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerInside && Input.GetKeyDown(KeyCode.F))
+            action.Action();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            playerInside = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            playerInside = false;
+    }
+}
