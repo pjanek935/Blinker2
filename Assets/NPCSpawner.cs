2c04bf2676a1d1ba13cbad0d49dae4542e9a68cd
Piotr <janek935@gmail.com>
2017-09-17 15:18:41 +0200

    NavMesh + NPC

---
+++ Assets/NPCSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSpawner : MonoBehaviour {
+
+    public GameObject npc;
+    public int amount = 10;
+
+	// Use this for initialization
+	void Start () {
+        GameObject poiContener = GameObject.Find("POI");
+        int childCount = poiContener.transform.childCount;
+
+        int npcCount = 0;
+
+        while (npcCount < amount)
+        {
+            for (int i = 0; i < childCount; i++)
+            {
+                GameObject npcInstance = Instantiate(npc);
+                npcInstance.transform.position = poiContener.transform.GetChild(i).transform.position;
+                npcCount++;
+
+                if (npcCount >= amount)
+                    break;
+
+            }
+        }
+          
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
